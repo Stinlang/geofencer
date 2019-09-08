9544b6d7a23b5e3a08b40c75ee65e923f05faf52
Stinlang <Stinlang@gmail.com>
2019-09-07 17:05:22 -0700

    Add feature selection functionality

--- src/components/app-container.tsx
+++ src/components/app-container.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 import BaseMap from './base-map';
 
+const ScrollableContainer = styled.div`
+  overflow: scroll;
+`;
+
 const AppContainer = () => (
-  <>
+  <ScrollableContainer>
     <BaseMap />
-  </>
+  </ScrollableContainer>
 );
 
 export default AppContainer;
--- src/components/base-map.tsx
+++ src/components/base-map.tsx
@@ -1,8 +1,20 @@
 import DeckGL from '@deck.gl/react';
 import { EditableGeoJsonLayer } from '@nebula.gl/layers';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StaticMap } from 'react-map-gl';
+import { Geometry } from 'wkx';
 import { PUBLIC_MAPBOX_TOKEN } from '../config';
+
+interface Pick {
+  object: { type: string };
+  index: number;
+}
+
+interface LayerEvent {
+  groundCoords: [number, number];
+  picks: Array<Pick>;
+  screenCoords: [number, number];
+}
 
 const initialViewState = {
   bearing: 0,
@@ -17,14 +29,67 @@ const BaseMap = () => {
     features: [],
     type: 'FeatureCollection',
   });
+  const [mode, setMode] = useState<string>('drawPolygon');
+  const [selectedFeatureIndexes, setSelectedFeatureIndexes] = useState<
+    Array<number>
+  >([]);
+
+  const getSelectedGeometry: any = () => {
+    const selectedFeature: any = data.features[selectedFeatureIndexes[0]];
+    return selectedFeature ? selectedFeature.geometry : null;
+  };
+
+  useEffect(() => {
+    const geo = getSelectedGeometry();
+    if (geo) {
+      console.log(geo);
+      console.log(Geometry.parseGeoJSON(geo).toWkt());
+    }
+  }, [selectedFeatureIndexes]);
+
+  class CustomEditableGeoJsonLayer extends EditableGeoJsonLayer {
+    constructor(props: any) {
+      super(props);
+    }
+
+    protected onLayerClick(e: LayerEvent) {
+      if (this.isFeaturePicked(e.picks)) {
+        const selectedIndexes = e.picks.map(pick => pick.index);
+        setSelectedFeatureIndexes(selectedIndexes);
+        if (this.props.mode !== 'translate') {
+          setMode('translate');
+        }
+      } else {
+        if (selectedFeatureIndexes.length) {
+          setSelectedFeatureIndexes([]);
+        }
+        setMode('drawPolygon');
+      }
+      super.onLayerClick(e);
+    }
+
+    private isFeaturePicked(picks: Array<Pick>) {
+      if (!picks.length) {
+        return false;
+      }
+      return picks.filter((pick: Pick) => pick.object.type === 'Feature')
+        .length;
+    }
+  }
 
   const layers = [
-    new EditableGeoJsonLayer({
+    new CustomEditableGeoJsonLayer({
       data,
-      id: 'geojson-layer',
-      mode: 'drawPolygon',
+      getFillColor: (_: any, isSelected: boolean) =>
+        isSelected ? [255, 0, 0, 150] : [0, 0, 0, 150],
+      getLineColor: (_: any, isSelected: boolean) =>
+        isSelected ? [200, 0, 0, 255] : [0, 0, 0, 255],
+      getLineDashArray: () => [0, 0],
+      getTentativeLineColor: () => [10, 0, 0, 200],
+      id: 'selected-features-layer',
+      mode,
       onEdit: ({ updatedData }: any) => setData(updatedData),
-      selectedFeatureIndexes: [],
+      selectedFeatureIndexes,
     }),
   ];
 
